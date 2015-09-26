893558b97be87c68cd73fa4a0ced8ab135f3fd1d
Pratheep <pratheep1231kumar@gmail.com>
2015-09-26 07:34:50 +0530

    Added google address maps.

--- views/contact.php
+++ views/contact.php
@@ -4,21 +4,36 @@
 <!-- Header ends -->
 
 <div id="inner_content">
- 	<h2>Contact</h2>
+ 	<h2>Contact Us</h2>
     <div id="uwild_scope_img">
 		<img src="../images/banner.jpg" alt="UWILD Scope and Execution">
 	</div>        
-    <h5>ABERDEN	:</h5>
     <div id="contact_inner_main_content">
-        <p align="justify">23 ELLON ROAD,</p> 
-        <p align="justify">ABERDEEN- AB23 8ED,</p>
-        <p align="justify">UNITED KINGDOM</p>
-        <h5>INDIA :</h5>
-        <p align="justify">PLOT NO.85, VASANTH NAGAR ROAD,</p> 
-        <p align="justify">NEAR IX PHASE, KPHB COLONY,</p>
-        <p align="justify">HYDERABAD-072. T.S.</p>
-        <p align="justify">MOBILE: +91 9908928984</p>
-        <p align="justify">EMAIL: MPPSINNOVATORS@GMAIL.COM</p>
+    	<table id="about_us_table" class="display">
+        	<tr class="table_color">
+            	<td>
+                	<h5>ABERDEN	:</h5>
+			        <p align="justify">23 ELLON ROAD,</p> 
+    	    		<p align="justify">ABERDEEN- AB23 8ED,</p>
+			        <p align="justify">UNITED KINGDOM</p>
+                </td>
+                <td>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2162.5854503739993!2d-2.0905776!3d57.1783326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48840de0bd9674dd%3A0x7db7f44bc5d62ca7!2s23+Ellon+Rd%2C+Bridge+of+Don%2C+Aberdeen%2C+Aberdeen+City+AB23+8ED%2C+UK!5e0!3m2!1sen!2sin!4v1443232806271" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </td>
+            <tr class="table_color">
+            	<td>
+        			<h5>INDIA :</h5>
+		        	<p align="justify">PLOT NO.85, VASANTH NAGAR ROAD,</p> 
+	        		<p align="justify">NEAR IX PHASE, KPHB COLONY,</p>
+			        <p align="justify">HYDERABAD-072. T.S.</p>
+        			<p align="justify">MOBILE: +91 9908928984</p>
+		        	<p align="justify">EMAIL: MPPSINNOVATORS@GMAIL.COM</p>
+                 </td>
+                 <td>
+			        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.3734977701506!2d78.38221719999999!3d17.4896753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb922045229131%3A0xc7473d3818b53676!2sHIG-83%2C+VNR+Residency%2C+Bagath+Singh+Nagar+Phase+2%2C+Kukatpally%2C+Hyderabad%2C+Telangana+500085!5e0!3m2!1sen!2sin!4v1443232169073" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                 </td>
+             </tr>
+          </table>
     </div>
 </div>
     
