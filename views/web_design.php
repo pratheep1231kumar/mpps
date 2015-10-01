b29e36b29e1afec558a50823dda816203426d644
Pratheep <pratheep1231kumar@gmail.com>
2015-10-01 23:19:25 +0530

    Corrected spelling.

--- views/web_design.php
+++ views/web_design.php
@@ -11,7 +11,7 @@
 	<h5>Web Site management :</h5>
     <ul class="bullet">
          <li>Web Design solutions including Graphic Design, Domain registration, Web Hosting,
-			SEO and Flsh Animation Ads</li>
+			SEO and Flash Animation Ads</li>
         <li>Print Media Services including Graphic Design, Package Design, Poster Design, Brand Identity
 			for Carporate companies</li>
         <li>Online Advertising for Social Media</li>
